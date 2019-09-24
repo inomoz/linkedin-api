13d1f01c5a788b7f23bc999491ce87eaecc181a2
Inom T <i@inomoz.ru>
2019-09-24 03:44:31 +0300

    remove unused settings

--- linkedin_api/settings.py
+++ linkedin_api/settings.py
@@ -5,26 +5,4 @@ HOMEPAGE_URL = 'https://www.linkedin.com/uas/login?trk=guest_homepage-basic_nav-
 HOMEPAGE_SALES_URL = 'https://www.linkedin.com/uas/login?session_redirect=%2Fsales&fromSignIn=true&trk=navigator'
 LOGIN_URL = 'https://www.linkedin.com/checkpoint/lg/login-submit'
 CHECKPOINT_URL = 'https://www.linkedin.com/checkpoint/challenge/verify'
-
-ADDITONAL_USER_AGENTS = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.3 Safari/605.1.15'
-]
-
-KEYORDER = ['csrfToken',
-            'session_key',
-            'ac',
-            'sIdString',
-            'controlId',
-            'parentPageKey',
-            'pageInstance',
-            'trk',
-            'session_redirect',
-            'loginCsrfParam',
-            'fp_data',
-            '_d',
-            '_f',
-            'session_password']
-
 LOGIN_TIMEOUT = 320
