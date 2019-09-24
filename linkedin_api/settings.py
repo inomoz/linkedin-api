c050bb20f565cae83a655ad7d3773cedb2c14713
Inom T <i@inomoz.ru>
2019-09-24 03:46:16 +0300

    remove unused urls

--- linkedin_api/settings.py
+++ linkedin_api/settings.py
@@ -1,8 +1,4 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-HOMEPAGE_URL = 'https://www.linkedin.com/uas/login?trk=guest_homepage-basic_nav-header-signin'
-HOMEPAGE_SALES_URL = 'https://www.linkedin.com/uas/login?session_redirect=%2Fsales&fromSignIn=true&trk=navigator'
-LOGIN_URL = 'https://www.linkedin.com/checkpoint/lg/login-submit'
-CHECKPOINT_URL = 'https://www.linkedin.com/checkpoint/challenge/verify'
 LOGIN_TIMEOUT = 320
